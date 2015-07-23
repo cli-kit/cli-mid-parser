1647bb248441849f5da4e98e19ef7cbea27a1e4b
muji <noop@xpm.io>
2015-07-23 11:15:33 +0800

    Update dep, hardcode --color flag.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var merge = require('cli-util').merge
  */
 function getParserConfiguration(target, config) {
   target = target || this;
-  config = config || {alias: {}, flags: [], options: []};
+  config = config || {alias: {}, flags: ['--color'], options: []};
   var k, arg, key
     , conf  = this.configure();
   var no = /^\[?no\]?/, nor = /(\[no-?\]-?)/;
